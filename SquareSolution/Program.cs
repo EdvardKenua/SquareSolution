73eb6cf4b05db7d1bf79d014cc82c36e75d7892c
EdvardKenua <orekhovsemyon@gmail.com>
2022-03-04 22:41:07 +0300

    Рекурсия. Завершена.

--- SquareSolution/Program.cs
+++ SquareSolution/Program.cs
@@ -6,38 +6,31 @@ namespace SquareSolution // Note: actual namespace depends on the project name.
 {
     public class Program
     {
-        //public static double E = 0.05, z = 1, y = 1, x = 0, k = 2;
-        //public static double Sqrt(double n)
-        //{
-        //    z = y;
-        //    y = (1 / k) * ((k - 1) * z + (x / Math.Pow(z, k - 1))); TODO: Доработать формулу
-        //    Console.WriteLine(y);
-        //    if (Math.Abs(y - z) > E)
-        //    {
-        //        return Sqrt(y);
-        //    }
-        //    else
-        //    {
-        //        return 0;
-        //    }
-        //}
+        public static double prevNum = 1, NumToSqrt = 0, CurNum = 1, k = 2, E = 0.05;
+
+        public static double Sqrt(double n)
+        {
+            prevNum = CurNum;
+            CurNum = (1 / k) * ((k - 1) * prevNum + (NumToSqrt / Math.Pow(prevNum, k - 1)));
+            Console.WriteLine(CurNum);
+            if (Math.Abs(CurNum - prevNum) > E)
+            {
+                return Sqrt(CurNum);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
 
 
         public static void Main(string[] args)
         {
-            double prevNum = 1, NumToSqrt = 0, CurNum = 1, k = 2, E = 0.05;
-            Console.WriteLine("Введите X: ");
+            Console.WriteLine("Введите число:");
             NumToSqrt = Convert.ToDouble(Console.ReadLine());
+            Sqrt(NumToSqrt);
 
-            if (NumToSqrt > 0)
-            {
-                do
-                {
-                    prevNum = CurNum;
-                    CurNum = (1 / k) * ((k - 1) * prevNum + (NumToSqrt / Math.Pow(prevNum, k-1)));
-                    Console.WriteLine(CurNum);
-                } while (Math.Abs(CurNum - prevNum) > E);
-            }
         }
     }
 }
