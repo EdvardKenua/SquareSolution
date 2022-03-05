e946db358e21d4347af7252687a0b51490b54657
Edvard Kenua <orekhovsemyon@gmail.com>
2022-03-05 10:08:31 +0300

    Добавление пользовательского кода ввода корня

--- SquareSolution/Program.cs
+++ SquareSolution/Program.cs
@@ -6,16 +6,16 @@ namespace SquareSolution // Note: actual namespace depends on the project name.
 {
     public class Program
     {
-        public static double prevNum = 1, NumToSqrt = 0, CurNum = 1, k = 2, E = 0.05;
+        public static double prevNum = 1, NumToSqrt = 0, CurNum = 1, E = 0.05;
 
-        public static double Sqrt(double n)
+        public static double Sqrt(double n, double k)
         {
             prevNum = CurNum;
             CurNum = (1 / k) * ((k - 1) * prevNum + (NumToSqrt / Math.Pow(prevNum, k - 1)));
             Console.WriteLine(CurNum);
             if (Math.Abs(CurNum - prevNum) > E)
             {
-                return Sqrt(CurNum);
+                return Sqrt(CurNum, k);
             }
             else
             {
@@ -29,7 +29,8 @@ namespace SquareSolution // Note: actual namespace depends on the project name.
         {
             Console.WriteLine("Введите число:");
             NumToSqrt = Convert.ToDouble(Console.ReadLine());
-            Sqrt(NumToSqrt);
+            int Root = 3;
+            Sqrt(NumToSqrt, Root);
 
         }
     }
